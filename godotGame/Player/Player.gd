211ca741a8b91584e7aa762f843ec5d73e1326b4
Obskera <vivifydecepi@gmail.com>
2023-01-11 23:57:11 -0800

    1. fixed double jump limiter; 2. cleaned up jump code; 3. added gravity machine implimentation; known issues: modifying jump force alone doesn't change jump height--eventually I need to adjust how the gravity is working to make that work correctly with the bound value.

--- godotGame/Player/Player.gd
+++ godotGame/Player/Player.gd
@@ -1,7 +1,22 @@
 extends KinematicBody2D
 class_name Player
+### BUG AREA: {X = fixed, O = open}
+### [O] Editor bound jump force not affecting jump height.
+### 	-> I suspect this is with the variable gravity implimentation, and will revisit that
+###		-> everything else is working rn so not high priority at the moment.
+### [X]  Double jump limiter breaking / Infinite Jumps:
+### 	-> Was being caused in a couple of parts, one the coyote timer was not working as intended
+### 	-> and also some of the other checks were overriding each other.
+### 	-> I condensed the jump functions into one larger one (though kept a few elements abstracted,
+### 	-> and was able to get the jump limiting workinbg correctly
+###
+### TODO AREA: {X = done, O = open, H = held, W = working}
+### [X] Make gravity machine?
+### 		-> pass GravityState.X where x = enum value into apply_gravity(delta, gravity_state)
+### 		-> This will make it a LOT easier to add and change various gravity modifying effects in the future.
 
 enum { MOVE, CLIMB }
+enum GravityState {GRAVITY, FAST_FALL, SLAM, JUMP_RELEASE, NEUTRAL}
 
 export(Resource) var moveData = preload("res://Player/DefaultPlayerMovementData.tres") as PlayerMovementData
 
@@ -18,10 +33,13 @@ var snap_vector = snapping
 
 var velocity = Vector2.ZERO
 var state = MOVE
+var gravity_state = GravityState.GRAVITY
 var double_jump = 1
 var buffered_jump = false
 var coyote_jump = false
 var on_door = false
+var jump_limiter = true
+var was_on_ground = true
 
 onready var animatedSprite: = $AnimatedSprite
 onready var ladderCheck: = $LadderCheck
@@ -40,72 +58,6 @@ func _physics_process(delta):
 		MOVE: move_state(input, delta)
 		CLIMB: climb_state(input)
 
-func move_state(input, delta):
-	slam(delta)
-	if is_on_floor() and Input.is_action_pressed("ui_up") and can_jump():
-		pogo_jump()
-		
-	if is_on_ladder() and Input.is_action_just_pressed("ui_up"):
-		state = CLIMB
-	
-	#change to gravity machine implimentation
-	apply_gravity(delta)
-	
-	if not horizontal_move(input):
-		apply_friction(delta)
-#		if not idleReset.is_stopped():
-#		animatedSprite.animation = "Idle"
-		set_idle()
-	else:
-		apply_acceleration(input.x, delta)
-#		animatedSprite.animation = "Run"
-		set_run()
-		animatedSprite.flip_h = !input.x > 0
-	
-	if is_on_floor():
-		
-		reset_double_jump()
-	else:
-#		animatedSprite.animation = "Jump"
-		set_jump()
-		
-	
-	if can_jump():
-		input_jump()
-	else:
-		input_jump_release()
-		input_double_jump()
-		buffer_jump()
-		pogo_jump()
-		fast_fall(delta)
-
-	var was_in_air = not is_on_floor()
-	var was_on_floor = is_on_floor()
-	
-#	if was_in_air and is_on_floor() and velocity.x <= 0:
-#		set_idle()
-
-	#OLD WORKING
-#	velocity = move_and_slide(velocity, FLOOR_NORMAL, true)
-	#NEW
-	velocity.y = move_and_slide_with_snap(velocity, snap_vector, FLOOR_NORMAL, true).y
-	player_brake()
-	
-	
-	var just_landed = is_on_floor() and was_in_air
-	if just_landed and velocity.x != 0:
-#		print('animation frame over ride')
-		set_run()
-#		animatedSprite.animation = "Run"
-		animatedSprite.frame = 1
-	elif just_landed and velocity.x == 0:
-		set_idle()
-	
-	var just_left_ground = not is_on_floor() and was_on_floor
-	if just_left_ground and velocity.y >= 0:
-		coyote_jump = true
-		coyoteJumpTimer.start()
-
 func climb_state(input):
 	if not is_on_ladder(): state = MOVE
 	if input.length() != 0:
@@ -116,12 +68,65 @@ func climb_state(input):
 		set_idle()
 	velocity = input * moveData.CLIMB_SPEED
 	velocity = move_and_slide(velocity, Vector2.UP)
-
+func move_state(input, delta):
+	#general stuff
+	jump_handler(delta)
+	slam(delta)
+	#change state in state machine when implimented
+	if is_on_ladder() and Input.is_action_just_pressed("ui_up"):
+		state = CLIMB
+	#change to gravity machine implimentation?
+	apply_gravity(delta, GravityState.GRAVITY)
+	#horizontal movement and friction
+	if not horizontal_move(input):
+		apply_friction(delta)
+		set_idle()
+	else:
+		apply_acceleration(input.x, delta)
+		set_run()
+		animatedSprite.flip_h = !input.x > 0
+	#moves player
+	velocity.y = move_and_slide_with_snap(velocity, snap_vector, FLOOR_NORMAL, true).y
+	#allow reseting horizontal momentum when changing direction
+	player_brake()
+	#checks
+	var was_in_air = not is_on_floor()
+	var was_on_floor = is_on_floor()
+	var just_landed = is_on_floor() and was_in_air
+	#set animation based on checks
+	if just_landed and velocity.x != 0:
+		set_run()
+		animatedSprite.frame = 1
+	elif just_landed and velocity.x == 0:
+		set_idle()
+func jump_handler(delta):
+	#RESET JUMP
+	if is_on_floor():
+#		reset_double_jump()
+		double_jump = moveData.DOUBLE_JUMP_COUNT
+		was_on_ground = true
+		jump_limiter = false
+		#check buffer jump:
+		if buffered_jump:
+			buffered_jump = false
+			jump()
+	else:
+		#sets sprite animation
+		set_jump()
+	#COYOTE
+	if (Input.is_action_just_pressed("ui_up") and can_jump() and double_jump >= 0) or (Input.is_action_just_pressed("ui_up") and !coyoteJumpTimer.is_stopped() and double_jump >= 0):
+		jump()
+	else:
+		input_jump_release(delta)
+		if Input.is_action_just_pressed("ui_up") and double_jump >= 0:
+			jump()
+		pogo_jump()
+		fast_fall(delta)
+	buffered_jump = false
 func set_idle():
 	if is_on_floor():
 		animatedSprite.animation = "Idle"
 func set_jump():
-#	yield(get_tree().create_timer(0.01), "timeout")
 	animatedSprite.animation = "Jump"
 func set_run():
 	if is_on_floor():
@@ -130,12 +135,9 @@ func player_die():
 	SoundPlayer.play_sound(SoundPlayer.HURT)
 	queue_free()
 	Events.emit_signal("player_died")
-
-#not yet working: for some reason the fall gravity is not applying even
-#at absurd numbers.
 func slam(delta):
 	if Input.is_action_just_pressed("ui_down") and !is_on_floor():
-		velocity.y += moveData.GRAVITY * 50000 * delta
+		apply_gravity(delta, GravityState.SLAM)
 func connect_camera(camera):
 	print('connecting camera')
 	var camera_path = camera.get_path()
@@ -145,6 +147,8 @@ func player_brake():
 	if Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_right"):
 		velocity.x = 0
 func jump():
+#	door override
+	if on_door: return
 	snap_vector = not_snapping
 	SoundPlayer.play_sound(SoundPlayer.JUMP)
 	#may need to account for variable jump height below
@@ -152,68 +156,56 @@ func jump():
 	double_jump -= 1
 	yield(get_tree().create_timer(.01), 'timeout')
 	snap_vector = snapping
-	
-	
+func can_jump():
+	var can_i_jump = false
+	if is_on_floor(): can_i_jump = true
+	if double_jump > 0: can_i_jump = true
+	return can_i_jump
 func pogo_jump():
-	if is_on_floor() and Input.is_action_pressed("ui_up") and can_jump():
+	if (is_on_floor() and Input.is_action_pressed("ui_up") and can_jump()):
 		jump()
-#		SoundPlayer.play_sound(SoundPlayer.JUMP)
-#		velocity.y = moveData.JUMP_FORCE
-#		double_jump -= 1
-func input_jump_release():
+##applies variable jump height
+func input_jump_release(delta):
 	if Input.is_action_just_released("ui_up") and velocity.y < moveData.JUMP_RELEASE_FORCE:
-		velocity.y = moveData.JUMP_RELEASE_FORCE
-
-func input_double_jump():
-	if Input.is_action_just_pressed("ui_up") and double_jump > 0:
-		buffered_jump = false
-		SoundPlayer.play_sound(SoundPlayer.JUMP)
-		velocity.y = moveData.JUMP_FORCE
-		double_jump -= 1
-
+		apply_gravity(delta, GravityState.JUMP_RELEASE)
+#for near ground jump input hits
 func buffer_jump():
 	if Input.is_action_just_pressed("ui_up") and not is_on_floor() and not jumpBufferTimer.is_stopped():
 		buffered_jump = true
 		jumpBufferTimer.start()
-
 func fast_fall(delta):
-	if velocity.y > 0:
-		velocity.y += moveData.ADDITIONAL_FALL_GRAVITY * delta
-
-func can_jump():
-	return is_on_floor() or coyote_jump
-
+	if !is_on_floor() and Input.is_action_pressed("ui_up"):
+		apply_gravity(delta, GravityState.FAST_FALL)
 func horizontal_move(input):
 	return input.x != 0
-
-func reset_double_jump():
-	double_jump = moveData.DOUBLE_JUMP_COUNT
-
-func input_jump():
-	if on_door: return
-	if Input.is_action_just_pressed("ui_up") or buffered_jump:
-		SoundPlayer.play_sound(SoundPlayer.JUMP)
-		velocity.y = moveData.JUMP_FORCE
-		buffered_jump = false
-
 func is_on_ladder():
 	if not ladderCheck.is_colliding(): return false
 	var collider = ladderCheck.get_collider()
 	if not collider is Ladder: return false
 	return true
-
-func apply_gravity(delta):
-	velocity.y += moveData.GRAVITY * delta
-	velocity.y = min(velocity.y, 700)
-
+func apply_gravity(delta, gravity_state):
+		var internal_gravity_state = gravity_state
+		match internal_gravity_state:
+			GravityState.GRAVITY: 
+				velocity.y += moveData.GRAVITY * delta
+				velocity.y = min(velocity.y, 700)
+			GravityState.FAST_FALL: 
+				if velocity.y < 0:
+					velocity.y += moveData.ADDITIONAL_FALL_GRAVITY * delta
+			GravityState.SLAM: 
+				velocity.y += moveData.GRAVITY * 5000 * delta
+			GravityState.JUMP_RELEASE: 
+				velocity.y = moveData.JUMP_RELEASE_FORCE
+			GravityState.NEUTRAL:
+				velocity.y = 0
+func normal_gravity(delta):
+	apply_gravity(delta, GravityState.GRAVITY)
 func apply_friction(delta):
 	velocity.x = move_toward(velocity.x, 0, moveData.FRICION * delta)
-
 func apply_acceleration(amount, delta):
 	velocity.x = move_toward(velocity.x, moveData.MAX_SPEED * amount, moveData.ACCELERATION * delta)
-
 func _on_JumpBufferTimer_timeout():
 	buffered_jump = false
-
 func _on_CoyoteJumpTimer_timeout():
 	coyote_jump = false
+	jump_limiter = true
