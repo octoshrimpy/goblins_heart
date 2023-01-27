d98f637c6a2038dd4e3d09eadbb6c8b3b1c06589
Obskera <vivifydecepi@gmail.com>
2023-01-26 18:13:02 -0800

    basic bones for instanceable context switcher, working base

---
+++ godotGame/RobsTestLand/TestChestBlock.gd
@@ -0,0 +1,6 @@
+extends StaticBody2D
+class_name CHEST
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
---
+++ godotGame/RobsTestLand/TestNPCBlock.gd
@@ -0,0 +1,4 @@
+extends StaticBody2D
+class_name NPC
+func _ready():
+	pass # Replace with function body.
