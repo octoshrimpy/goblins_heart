03ba911b1b8b7d78c8084edacd475cc409d7b012
Obskera <vivifydecepi@gmail.com>
2023-01-26 22:23:35 -0800

    added all 3 context based effects; still need to add the menu pop-over when interacting, but logic is there for instanced visuals based on contextual distance

--- godotGame/ProximityContextPrompt.gd
+++ godotGame/ProximityContextPrompt.gd
@@ -2,13 +2,14 @@ extends Area2D
 #get parent node, will be node type/name unless class_name is declared
 onready var parent_context = get_parent()
 
-func _on_FarContextPrompt_body_entered(body):
+func _on_ProximityContextPrompt_body_entered(body):
 		#Match context of parent node name, and run whatever you put for it
 	if parent_context is NPC: 
-		print('NPC far context')
+		print('NPC proximity context')
 	elif parent_context is CHEST:
-		print('Chest far Context')
-		parent_context.glow()
+		print('Chest proximity Context')
+		parent_context.hide_arrow()
+		parent_context.show_interact()
 #	elif parent_context is PLAYER_OBSERVATION:
 #		print('Player makes a remark about what they observed')
 	else: pass
@@ -16,17 +17,20 @@ func _on_FarContextPrompt_body_entered(body):
 ##		body.player_interact()
 #		pass
 
-func _on_FarContextPrompt_body_exited(body):
+func _on_ProximityContextPrompt_body_exited(body):
 				#Match context of parent node name, and run whatever you put for it
 	if parent_context is NPC: 
-		print('NPC far context')
+		print('NPC proximity context')
 	elif parent_context is CHEST:
-		print('Chest far Context')
-		parent_context.not_glow()
+		print('Chest proximity Context')
+#		parent_context.not_glow()
+		parent_context.hide_interact()
+		parent_context.show_arrow()
 #	elif parent_context is PLAYER_OBSERVATION:
 #		print('Player makes a remark about what they observed')
 	else: pass
 #	if body is Player:
 ##		body.player_interact()
 #		pass
+
 
--- godotGame/RobsTestLand/TestChestBlock.gd
+++ godotGame/RobsTestLand/TestChestBlock.gd
@@ -2,20 +2,34 @@ extends StaticBody2D
 class_name CHEST
 onready var arrow = $Sprite2
 onready var glow = $GlowAnimation
+onready var interact = $InteractAnimation
+onready var interactSprite = $Interact
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+#Near prompt for arrows
 func show_arrow():
 	arrow.visible = true
 func hide_arrow():
 	arrow.visible = false
+
+#far prompt for glow
 func glow():
-	print('glow')
 	glow.play('glow')
 func not_glow():
-	print('no glow')
 	if glow.is_playing():
-		
 		glow.stop(true)
 		glow.seek(0, true)
+
+#proximity prompt for interact
+func show_interact():
+	interactSprite.visible = true
+	interact.play('Interact')
+func hide_interact():
+	if interact.is_playing():
+		interactSprite.visible = false
+		interact.stop()
+		interact.seek(0, true)
+	
